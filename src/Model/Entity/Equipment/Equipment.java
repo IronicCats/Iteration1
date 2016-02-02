cfca50c5c17ad590519101b7ff206585f38e06a5
Kyle Brost <kjbrost@gmail.com>
2016-02-01 19:32:28 -0500

    should have working: StatStructure and Stats, including Primary/Derived.  Others are works in progress.

---
+++ src/Model/Entity/Equipment/Equipment.java
@@ -0,0 +1,16 @@
+package Model.Entity.Equipment;
+
+import Model.Entity.EquipmentStats;
+
+/**
+ * Created by broskj on 2/1/16.
+ */
+public class Equipment {
+    //private EquippedWeapon weapon;
+    //private EquippedArmor armor;
+    private EquipmentStats equipmentStats;
+
+    Equipment() {
+
+    } // end constructor
+} // end class Equipment
