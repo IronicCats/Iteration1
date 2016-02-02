24197b473e8b322abdc600c0e9855c3ef50fa94d
Kyle Brost <kjbrost@gmail.com>
2016-02-01 20:52:17 -0500

    added classes pertaining to Inventory and Equipment

---
+++ src/Model/Entity/Inventory/Pack.java
@@ -0,0 +1,24 @@
+package Model.Entity.Inventory;
+
+/**
+ * Created by broskj on 2/1/16.
+ */
+public class Pack {
+    private int size;
+    private Item[] items;
+    private final int cap;
+
+    Pack(int cap) {
+        this.size = 0;
+        this.items = new Item[cap];
+        this.cap = cap;
+    } // end constructor
+
+    Pack (int size, Item[] items, int cap) {
+        this.size = size;
+        this.items = items;
+        this.cap = cap;
+    } // end constructor
+
+
+} // end class Pack
