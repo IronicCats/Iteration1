6758c7f9d048b10f871e20ead501033e01fff2c1
Peter Camejo <thealex123@gmail.com>
2016-02-03 20:54:24 -0500

    Revert "Added Obstacle.java. Added items[] attribute to Tile.java. Added constructors to Item.java"
    This reverts commit 1a0c0a89fa38c45fe5e5d0045812ef2e84db6792.

--- src/Model/Map/Tiles/Tile.java
+++ src/Model/Map/Tiles/Tile.java
@@ -1,8 +1,6 @@
 package Model.Map.Tiles;
 
 import Model.Location;
-import com.sun.org.apache.xpath.internal.operations.Mod;
-import sun.awt.image.BufferedImageDevice;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -18,20 +16,11 @@ public abstract class Tile {
     private Location location;
     private BufferedImage texture;
     public boolean isUnWalkable;
-    public Model.Item.Item[] items = new Model.Item.Item[10];
 
-    //Constructor
-    public Tile(BufferedImage texture, Location location , boolean isUnWalkable){
+    public  Tile(BufferedImage texture, Location location, boolean isUnWalkable) {
         this.location = location;
         this.texture = texture;
         this.isUnWalkable = isUnWalkable;
-    }
-    //Constructor with items[] added
-    public  Tile(BufferedImage texture, Location location, boolean isUnWalkable, Model.Item.Item[] items) {
-        this.location = location;
-        this.texture = texture;
-        this.isUnWalkable = isUnWalkable;
-        this.items = items;
     }
 
     public void render(Graphics g,int x, int y) {
