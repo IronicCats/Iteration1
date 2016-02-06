80b975f5a01ccd32e2475cf15caca906031fe95d
Andres Borjas <aborjas@ufl.edu>
2016-02-05 19:59:59 -0500

    Savestate and loadstate actual code

---
+++ src/Controller/States/LoadState.java
@@ -0,0 +1,51 @@
+package Controller.States;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+
+import Controller.Controller;
+
+/**
+ * Created by Andy on 2/4/2016.
+ */
+public class LoadState extends State{
+
+    public LoadState(Controller controller)
+    {
+        super(controller);
+
+    }
+
+    public void tick()
+    {
+
+    }
+
+    public void render(Graphics g)
+    {
+
+    }
+
+    public static void loadFile(String filepath){
+        File inputFile;
+        BufferedReader inputReader;
+
+        try{
+            inputFile = new File(filepath);
+            inputReader = new BufferedReader(new FileReader(inputFile));
+            String fileText = inputReader.readLine();
+            System.out.println(fileText);
+
+
+            inputReader.close();
+
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
