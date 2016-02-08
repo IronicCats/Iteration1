4dcf0b09aff7cc0cfe46c2b6f361eea645e9498b
Joshua Lee Kegley <josh1289op@gmail.com>
2016-02-08 02:19:15 -0500

    Fixing menu

--- src/View/Graphics/Assets.java
+++ src/View/Graphics/Assets.java
@@ -52,8 +52,15 @@ public class Assets {
     public static BufferedImage oneShot;
     public static BufferedImage damage;
 
+    public static BufferedImage title;
+    public static BufferedImage background;
+
 
     public static void init() {
+
+        title = new ImageLoader().loadImage("/textures/title.png");
+        background = new ImageLoader().loadImage("/textures/backgroundimage.png");
+
         SpriteSheet grassSheet = new SpriteSheet(new ImageLoader().loadImage("/textures/GrassTiles.png"));
         SpriteSheet mountainSheet = new SpriteSheet(new ImageLoader().loadImage("/textures/MountainTile.png"));
         SpriteSheet waterSheet = new SpriteSheet(new ImageLoader().loadImage("/textures/WaterTile.png"));
@@ -88,10 +95,16 @@ public class Assets {
         SpriteSheet buttonSheet = new SpriteSheet(new ImageLoader().loadImage("/textures/buttons.png"));
         SpriteSheet damageSheet = new SpriteSheet(new ImageLoader().loadImage("/textures/damage.png"));
 
+
+
+
         water = new ArrayList<>(9);
         mountain = new ArrayList<>(9);
         grass = new ArrayList<>(9);
         buttons = new ArrayList<>(4);
+
+
+
 
         avatarFacingDown = avatarSheet.crop(0, 0, width, height);
         avatarFacingUp = avatarSheet.crop(32,0,width,height);
--- src/View/Views/MainMenu.java
+++ src/View/Views/MainMenu.java
@@ -2,6 +2,7 @@ package View.Views;
 
 
 import Controller.States.States;
+import View.Graphics.Assets;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,6 +13,8 @@ import java.awt.*;
 public class MainMenu  {
 
     int width, height;
+    int redTitle, greenTitle, blueTitle, state;
+    int backX, backY;
 
     private String[] menuItems = {"Create Game", "Load Game", "Exit Game"};
     private States[] menuStates = {States.Create, States.Load, States.Exit};
@@ -20,28 +23,57 @@ public class MainMenu  {
     public MainMenu(int width, int height) {
         this.width = width;
         this.height = height;
+        this.redTitle = 255;
+        this.greenTitle = 0;
+        this.blueTitle = 0;
+        this.state = 0;
+        this.backX = 0;
+        this.backY = 0;
     }
 
     public void render(Graphics g) {
-        g.setColor(new Color(38, 166, 91));
-        g.fillRect(0, 0, width, height);
+
+        //g.setColor(new Color(38, 166, 91));
+        //g.fillRect(0, 0, width, height);
+        g.drawImage(Assets.background, backX, backY, 800, 600, null);
 
         for(int i = 0; i < menuItems.length; ++i) {
             g.setFont(new Font("Arial", Font.PLAIN, 54));
             FontMetrics fm = g.getFontMetrics();
             int totalWidth = (fm.stringWidth(menuItems[i]));
             int x = (width - totalWidth) / 2;
-            int y = (height / 2) - 100 + 100 * i;
+            int y = 50 + (height / 2) - 100 + 120 * i;
 
             if(i == currentItem){
-                g.setColor(Color.ORANGE);
+                g.setColor(new Color(149, 165, 166, 175));
                 g.fillRect(x, y - fm.getHeight() + (fm.getHeight() / 4), totalWidth, fm.getHeight() );
-                g.setColor(Color.PINK);
+                g.setColor(new Color(243, 156, 18));
+
             }else {
-                g.setColor(Color.GREEN);
+                g.setColor(new Color(231, 76, 60));
             }
             g.drawString(menuItems[i], x, y);
+
         }
+        changeColor();
+        Color color = new Color(redTitle, greenTitle, blueTitle);
+        int rgb = color.getRGB();
+        /*
+        for (int i = 0; i < Assets.title.getWidth(); i++) {
+            for (int j = 0; j < Assets.title.getHeight(); j++) {
+                Color c = new Color(Assets.title.getRGB(i, j));
+                if(c.getAlpha() >= 255){
+                    Assets.title.setRGB(i, j, rgb);
+                }
+
+            }
+        }
+        */
+        g.setFont(new Font("Arial", Font.PLAIN, 154));
+        g.setColor(color);
+        FontMetrics fm = g.getFontMetrics();
+        g.drawString("Ironic Cats",width / 2 - fm.stringWidth("Ironic Cats")/2, 25 + fm.getHeight()/2 );
+
     }
 
     public void next() {
@@ -60,6 +92,52 @@ public class MainMenu  {
 
     public States getSelection() {
         return menuStates[currentItem];
+    }
+
+    public void changeColor() {
+        switch(state){
+            case 0:
+                System.out.println("Green");
+                greenTitle += 5;
+                if(greenTitle == 255) {
+                    state = 1;
+                }
+                break;
+            case 1:
+                redTitle -=5;
+                System.out.println("-Red");
+                if( redTitle == 0){
+                    state = 2;
+                }
+                break;
+            case 2:
+                blueTitle += 5;
+                System.out.println("Blue");
+
+                if(blueTitle == 255) {
+                    state = 3;
+                }
+                break;
+            case 3:
+                greenTitle -= 5;
+                if(greenTitle == 0) {
+                    state = 4;
+                }
+                break;
+            case 4:
+                redTitle += 5;
+                if(redTitle == 255) {
+                    state = 5;
+                }
+                break;
+            case 5:
+                blueTitle -= 5;
+                if( blueTitle == 0) {
+                    state = 0;
+                }
+                break;
+
+        }
     }
 
 }
